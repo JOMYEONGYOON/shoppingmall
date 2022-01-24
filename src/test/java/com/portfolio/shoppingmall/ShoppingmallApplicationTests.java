f0b9a83aafc33bbeb9e9b2593f449dece4f42676
JMY <93877826+JOMYEONGYOON@users.noreply.github.com>
2022-01-24 16:49:33 +0900

    Initial commit

---
+++ src/test/java/com/portfolio/shoppingmall/ShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
