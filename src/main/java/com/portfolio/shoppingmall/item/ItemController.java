dce3652f42a1fbbbaca4678dd1ad9b2e8b64650b
JMY <93877826+JOMYEONGYOON@users.noreply.github.com>
2022-01-27 18:57:35 +0900

    2022/01/27 shoppingmall

---
+++ src/main/java/com/portfolio/shoppingmall/item/ItemController.java
@@ -0,0 +1,19 @@
+package com.portfolio.shoppingmall.item;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@Controller
+public class ItemController {
+
+    @GetMapping("/{itemId}")
+    public String itemView(@PathVariable long itemId, Model model){
+        return "item";
+    }
+
+
+}
---
+++ src/main/java/com/portfolio/shoppingmall/item/ItemRepository.java
@@ -0,0 +1,15 @@
+package com.portfolio.shoppingmall.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+
+}
