8be1c9f25a9888a5d08609c585145dc555a81e2e
JMY <93877826+JOMYEONGYOON@users.noreply.github.com>
2022-01-29 12:21:21 +0900

    2022/01/29 member

--- src/main/java/com/portfolio/shoppingmall/domain/item/Item.java
+++ src/main/java/com/portfolio/shoppingmall/domain/item/Item.java
@@ -1,5 +1,7 @@
 package com.portfolio.shoppingmall.domain.item;
 
+import com.portfolio.shoppingmall.domain.Category;
+import com.portfolio.shoppingmall.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.BatchSize;
@@ -22,26 +24,26 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
-//
-//    @ManyToMany(mappedBy = "items")
-//    private List<Category> categories = new ArrayList<>();
-//
-//    //==비즈니스 로직==//
-//    /**
-//     * stock 증가
-//     */
-//    public void addStock(int quantity) {
-//        this.stockQuantity += quantity;
-//    }
-//
-//    /**
-//     * stock 감소
-//     */
-//    public void removeStock(int quantity) {
-//        int restStock = this.stockQuantity - quantity;
-//        if (restStock < 0) {
-//            throw new NotEnoughStockException("need more stock");
-//        }
-//        this.stockQuantity = restStock;
-//    }
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
