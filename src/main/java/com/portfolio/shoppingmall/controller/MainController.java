69f7af586258892d5ab731b33906fd8211f60260
JMY <93877826+JOMYEONGYOON@users.noreply.github.com>
2022-01-30 16:17:40 +0900

    2022/01/30 memberform

---
+++ src/main/java/com/portfolio/shoppingmall/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.portfolio.shoppingmall.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+
+    @RequestMapping("/")
+    public String home(){
+        log.info("home Controller");
+        return "home";
+    }
+
+
+}
