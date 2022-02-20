5fa645aec4a5cb07cb1c4f3e108f2a0fd260ac24
JMY <93877826+JOMYEONGYOON@users.noreply.github.com>
2022-02-20 16:12:59 +0900

    2022/02/20 order service

--- src/main/java/com/portfolio/shoppingmall/domain/member/Member.java
+++ src/main/java/com/portfolio/shoppingmall/domain/member/Member.java
@@ -2,24 +2,26 @@ package com.portfolio.shoppingmall.domain.member;
 
 import com.portfolio.shoppingmall.dto.MemberDto;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Collection;
 
 @AllArgsConstructor
 @Entity
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 //@Builder(builderMethodName = "MemberBuilder")
-public class Member {
+public class Member implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String email; // 로그인 아이디
+    @Column(name = "email", unique = true)
+    private String email;
 
+    @Column(name = "password")
     private String password;
 
     private String name;
@@ -27,6 +29,51 @@ public class Member {
     private String phone;
 
     private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    // 사용자의 id를 반환 (unique한 값)
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    // 사용자의 password를 반환
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    // 계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        // 만료되었는지 확인하는 로직
+        return true; // true -> 만료되지 않았음
+    }
+
+    // 계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        // 계정 잠금되었는지 확인하는 로직
+        return true; // true -> 잠금되지 않았음
+    }
+
+    // 패스워드의 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // 패스워드가 만료되었는지 확인하는 로직
+        return true; // true -> 만료되지 않았음
+    }
+
+    // 계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled() {
+        // 계정이 사용 가능한지 확인하는 로직
+        return true; // true -> 사용 가능
+    }
 
 //
 //    public static MemberBuilder builder(MemberDto memberDto) {
